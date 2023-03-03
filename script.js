843a1e01f82897f5d9ca224ee3d51a2bbac6c3f0
manitofighh <manitofigh@protonmail.com>
2023-03-02 19:28:35 -0500

    ch

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var level = 1;
-var startState = false;
+var level = 0;
+var started = false;
 var gameOrder = [];
-var playerOrder = [];
+var userOrder = [];
 var b1Sound = new Audio("Sounds/b1.mp3");
 var b2Sound = new Audio("Sounds/b2.mp3");
 var b3Sound = new Audio("Sounds/b3.mp3");
@@ -9,57 +9,64 @@ var b4Sound = new Audio("Sounds/b4.mp3");
 var wrongSound = new Audio("Sounds/wrong.mp3");
 
 $(document).keydown(function(event){
-    if (event.which === 13 && startState === false){
-        $("h1").css("color", "white").text("Level " + level);
-        startState = true;
+    if (event.which === 13 && started === false){
+        $("h1").text("Level " + level);
+        started = true;
         startGame();
+        // startGame();
     }
 });
 
-
-function startGame(){  
-    if (startState){  
+function startGame(){
+    if (started) {
         number = Math.floor(Math.random() * 4) + 1;
-        thisClass = ".b" + number;
-        setTimeout(function(){
-        $(thisClass).css("opacity", 0.5);
-        switch (number) {
-            case 1:
-                b1Sound.play();
-                break;
-            case 2:
-                b2Sound.play();
-                break;
-            case 3:
-                b3Sound.play();
-            case 4:
-                b4Sound.play();
-            default:
-                break;
-        }
+        $(".b" + number).addClass("button-flash");
+        setTimeout(() => $(".b" + number).removeClass("button-flash"), 200);
         gameOrder.push(number);
-        }, 300);
-        setTimeout(() => $(thisClass).css("opacity", 1), 600);
-        $(".button").click(function(){
-            if ($(this).hasClass("b" + number)){
-                level += 1;
-                $("h1").text("Level " + level);
-                startGame();
-            }
-            else {
-                $("body").css("background-color", "red");
-                wrongSound.play();
-                setTimeout(() => $("body").css("background-color", "#1b1d1c"), 120);
-                $("h1").css("color", "red").text("Game Over! Press the \"Enter\" key to restart.")
-                level = 0;
-                gameOrder = [];
-                playerOrder = [];
-                startState = false;
-            }
-            
-        });
-    }  
-
-
-
+        }
 }
+
+
+// function startGame(){  
+//     if (started){  
+//         number = Math.floor(Math.random() * 4) + 1;
+//         thisClass = ".b" + number;
+//         setTimeout(function(){
+//         $(thisClass).css("opacity", 0.5);
+//         switch (number) {
+//             case 1:
+//                 b1Sound.play();
+//                 break;
+//             case 2:
+//                 b2Sound.play();
+//                 break;
+//             case 3:
+//                 b3Sound.play();
+//             case 4:
+//                 b4Sound.play();
+//             default:
+//                 break;
+//         }
+//         gameOrder.push(number);
+//         }, 300);
+//         setTimeout(() => $(thisClass).css("opacity", 1), 600);
+//         $(".button").click(function(){
+//             if ($(this).hasClass("b" + number)){
+//                 level += 1;
+//                 $("h1").text("Level " + level);
+//                 startGame();
+//             }
+//             else {
+//                 $("body").css("background-color", "red");
+//                 wrongSound.play();
+//                 setTimeout(() => $("body").css("background-color", "#1b1d1c"), 120);
+//                 $("h1").css("color", "red").text("Game Over! Press the \"Enter\" key to restart.")
+//                 level = 0;
+//                 gameOrder = [];
+//                 playerOrder = [];
+//                 started = false;
+//             }
+            
+//         });
+//     }  
+//}
